931d9f6669ca983b61b84d2789334f48da320610
TKDKid1000 <rhoneeiler@gmail.com>
2021-03-16 21:21:07 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+// DO NOT REMOVE THIS
+var config = {};
+
+// the hostname the nodebin server runs on. for example: 127.0.0.1, 0.0.0.0, localhost, example.com
+config.hostname="localhost"
+// the port that the nodebin server runs on
+config.port=8080
+// the name of the nodebin. usually just called "nodebin"
+config.name="NodeBin"
+// the description of the nodebin. just any text you want to display with the nodebin.
+// use `text` for a multiline string.
+// full html works here too.
+config.description=`Hello! Welcome to NodeBin.
+NodeBin is the best pastebin.`
+
+// DO NOT REMOVE THIS
+module.exports = config;
---
+++ main.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const fs = require("fs");
+const url = require("url");
+const bodyParser = require("body-parser");
+const app = express();
+const pjson = require('./package.json');
+const config = require("./config");
+
+app.set('view engine', 'ejs')
+app.set('views', "./template")
+app.use("/static", express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get("", (request, response) => {
+  if (request.query.paste === undefined) {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/html");
+    response.render("index", {
+      name: config.name,
+      description: config.description,
+      version: pjson.version
+    });
+    response.end();
+  } else {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    var json = JSON.parse( fs.readFileSync("pastes.json", "utf-8"));
+    if (request.query.paste in json) {
+      response.write(json[request.query.paste]);
+      response.end();
+    } else {
+      response.write("null");
+      response.end();
+    }
+  }
+});
+
+app.post("", (request, response) => {
+  var content = request.body.content;
+  var id = makeid(10);
+  var json = JSON.parse(fs.readFileSync("pastes.json", "utf-8"));
+  json[id] = content;
+  fs.writeFileSync("pastes.json", JSON.stringify(json));
+  response.redirect("?paste="+id);
+  response.end();
+});
+
+app.listen(config.port, config.hostname, () => {
+  console.log(`Server running at http://${config.hostname}:${config.port}/`);
+});
+
+function makeid(length) {
+  var result           = "";
+  var characters       = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
